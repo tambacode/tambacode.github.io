886d021f1f10397770363556b96d021d60cd7009
franlima <fol@cesar.school>
2019-08-11 14:57:55 -0400

    Added right icon functionality [future]

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,5 +1,32 @@
 var ui_openSidebar = function() {
-    $('.ui.sidebar')
+    $('.ui.left.sidebar')
         .sidebar('setting', 'transition', 'overlay')
         .sidebar('toggle')
 };
+
+/*
+var ui_openFilterSidebar = function() {
+    $('.ui.right.sidebar')
+        .sidebar('setting', 'transition', 'overlay')
+        .sidebar('toggle')
+};
+*/
+
+var ui_headerRightAction = function(){
+
+	var ui_rightIcon = "<button class='ui icon button orange' onclick='{0}'> \
+                    		<i class='icon {1} large'></i> \
+                    	</button>";
+
+    if (localStorage.getItem('auth_UserOnline')) {
+        ui_rightIcon = ui_rightIcon.replace('{0}', 'misc_GoToHome();');
+        ui_rightIcon = ui_rightIcon.replace('{1}', 'user circle');
+        //ui_rightIcon = ui_rightIcon.replace('{0}', 'ui_openFilterSidebar();');
+        //ui_rightIcon = ui_rightIcon.replace('{1}', 'filter');
+    } else {
+        ui_rightIcon = ui_rightIcon.replace('{0}', 'misc_GoToHome();');
+        ui_rightIcon = ui_rightIcon.replace('{1}', 'user circle');
+    }
+
+	$('#header_right').append(ui_rightIcon);
+}
