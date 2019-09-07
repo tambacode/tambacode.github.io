2beccca82c23483245bb86147048006676fe08bd
franlima <fol@cesar.school>
2019-09-07 12:32:47 -0400

    Youtube videos needs "embed" to load correctly

--- public/js/ad.js
+++ public/js/ad.js
@@ -727,7 +727,7 @@ const ad_ValuesIntoDetail = function(val, icounter) {
         category.innerText = val.category;
         subcategory.innerText = val.subcategory;
         if (val.category === "eventos"){
-            ad_addSliderVideo(val.event_url);
+            ad_addSliderVideo("https://www.youtube.com/embed/" + val.event_url);
             datead.innerText = val.event_date;
             $("#urlad").prop("href", val.event_site);
             $('#adevents').removeClass('hidden');
