f69219d0059c2d987a9f126bf143ac729170dfd5
franlima <fol@cesar.school>
2019-09-18 05:08:45 -0400

    [836251][836252][836253] Added service worker and adjusted manifest.json

---
+++ public/sw.js
@@ -0,0 +1,65 @@
+const  version = "0.0.1";
+const cacheName = `sharefarm-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        "/index.html"
+      ])
+      .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cache) {
+          if (cache.startsWith('sharefarm-') && cacheName !== cache) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+        console.log('Found ', event.request.url, ' in cache');
+        return response || fetch(event.request);
+    })
+  );
+});
+
+/*
+self.addEventListener('fetch', function(event) {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request).then(function(response) {
+          if (response.status === 404) {
+            return caches.match('fourohfour.html');
+          }
+          return caches.open(cached_urls).then(function(cache) {
+           cache.put(event.request.url, response.clone());
+            return response;
+          });
+        });
+      }).catch(function(error) {
+        console.log('Error, ', error);
+        return caches.match('offline.html');
+      })
+    );
+  });
+*/
