059bb9095da7d6653e869e4b254450c5f289f754
franlima <fol@cesar.school>
2019-09-23 22:43:24 -0400

    [836251][836252][836253] Removed anoying console log message

--- public/sw.js
+++ public/sw.js
@@ -27,15 +27,12 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', event => {
-  //console.log('Fetch event for ', event.request.url);
   event.respondWith(
     caches.match(event.request)
     .then(response => {
       if (response) {
-        //console.log('Found ', event.request.url, ' in cache');
         return response;
       }
-      //console.log('Network request for ', event.request.url);
       return fetch(event.request).then(response => {
         if (response.status === 404) {
           return caches.match('/404.html');
